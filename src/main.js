53b3d8da256f9375dd6698d16f9296c96c9d5690
Vince Allen <vince@spotify.com>
2014-06-30 14:58:52 -0400

    Browserify compiles a standalone bundle.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-var Burner = {
+module.exports = {
   Item: require('./Item').Item,
   System: require('./System').System,
   Utils: require('./Utils').Utils,
   Vector: require('./Vector').Vector,
   World: require('./World').World
 };
-
-window.Burner = Burner;
